9658617b67bfba21e2fcfc349bd38f26bdad5a6c
adamzwickey <azwickey@vmware.com>
2020-11-04 13:55:29 -0500

    adding namespace automation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,33 +2,48 @@
 : ${VARS_YAML?"Need to set VARS_YAML environment variable"}
 echo config YAML:
 cat $VARS_YAML
-
-# Login
-# curl -vvv -k \
-#    -H "vmware-use-header-authn: application/json" \
-#    https://vcsa-01.haas-439.pez.vmware.com/rest/com/vmware/cis/session
-#curl -vvv -k c  https://vcsa-01.haas-439.pez.vmware.com/api/vcenter/namespaces/instances 
 export SUPERVISOR_VIP=$(yq r $VARS_YAML vsphere.supervisor-vip)
 export SUPERVISOR_USERNAME=$(yq r $VARS_YAML vsphere.username)
 export SUPERVISOR_PWD=$(yq r $VARS_YAML vsphere.password)
+export VCSA=$(yq r $VARS_YAML vsphere.host)
 kubectl vsphere login -v 8 --server=$SUPERVISOR_VIP --insecure-skip-tls-verify -u $SUPERVISOR_USERNAME
 
-# Namespaces
+# Create Namespaces
+python ./scripts/create-namespace.py -s $VCSA -u $SUPERVISOR_USERNAME -p $SUPERVISOR_PWD \
+       -cl $(yq r $VARS_YAML vsphere.clusterName) -role EDIT -st USER -subject $SUPERVISOR_USERNAME \
+       -domain $(echo $SUPERVISOR_USERNAME | cut -d "@" -f 2) -sp $(yq r $VARS_YAML shared-services.storage) \
+       -ns $(yq r $VARS_YAML shared-services.namespace)
+python ./scripts/create-namespace.py -s $VCSA -u $SUPERVISOR_USERNAME -p $SUPERVISOR_PWD \
+       -cl $(yq r $VARS_YAML vsphere.clusterName) -role EDIT -st USER -subject $SUPERVISOR_USERNAME \
+       -domain $(echo $SUPERVISOR_USERNAME | cut -d "@" -f 2) -sp $(yq r $VARS_YAML workload1.storage) \
+       -ns $(yq r $VARS_YAML workload1.namespace)
+python ./scripts/create-namespace.py -s $VCSA -u $SUPERVISOR_USERNAME -p $SUPERVISOR_PWD \
+       -cl $(yq r $VARS_YAML vsphere.clusterName) -role EDIT -st USER -subject $SUPERVISOR_USERNAME \
+       -domain $(echo $SUPERVISOR_USERNAME | cut -d "@" -f 2) -sp $(yq r $VARS_YAML workload2.storage) \
+       -ns $(yq r $VARS_YAML workload2.namespace)
 
 # Create Shared Services Cluster
-mkdir -p generated/shared 
-export SHARED_SERVICES_NS=$(yq r $VARS_YAML shared-services.namespace)
 kubectl config use-context $SUPERVISOR_VIP
-cp manifests/shared-services.yaml generated/shared/
-yq write generated/shared/shared-services.yaml -i "metadata.namespace" $SHARED_SERVICES_NS
-kubectl apply -f generated/shared/shared-services.yaml
-while kubectl get tanzukubernetesclusters shared-services -n $SHARED_SERVICES_NS | grep running ; [ $? -ne 0 ]; do
+mkdir -p generated/shared 
+cp cd/apps/workload-cluster/values.yaml generated/shared/values-shared-services.yaml
+yq write generated/shared/values-shared-services.yaml -i "name" $(yq r $VARS_YAML shared-services.name)
+yq write generated/shared/values-shared-services.yaml -i "namespace" $(yq r $VARS_YAML shared-services.namespace)
+yq write generated/shared/values-shared-services.yaml -i "kubernetesVersion" $(yq r $VARS_YAML shared-services.kubernetesVersion)
+yq write generated/shared/values-shared-services.yaml -i "storage" $(yq r $VARS_YAML shared-services.storage)
+yq write generated/shared/values-shared-services.yaml -i "controlPlaneCount" $(yq r $VARS_YAML shared-services.controlPlaneCount)
+yq write generated/shared/values-shared-services.yaml -i "controlPlaneClass" $(yq r $VARS_YAML shared-services.controlPlaneClass)
+yq write generated/shared/values-shared-services.yaml -i "workerCount" $(yq r $VARS_YAML shared-services.workerCount)
+yq write generated/shared/values-shared-services.yaml -i "workerClass" $(yq r $VARS_YAML shared-services.workerClass)
+helm install $(yq r $VARS_YAML shared-services.name) cd/apps/workload-cluster/ --namespace $(yq r $VARS_YAML shared-services.namespace) -f generated/shared/values-shared-services.yaml
+export SHARED_SERVICES_NS=$(yq r $VARS_YAML shared-services.namespace)
+export SHARED_SERVICES_NAME=$(yq r $VARS_YAML shared-services.name)
+while kubectl get tanzukubernetesclusters $SHARED_SERVICES_NAME -n $SHARED_SERVICES_NS | grep running ; [ $? -ne 0 ]; do
 	echo Shared Services Cluster is not Running...
 	sleep 5s
 done
 
 kubectl vsphere login -v 8 --server=$SUPERVISOR_VIP \
-   --tanzu-kubernetes-cluster-name shared-services \
+   --tanzu-kubernetes-cluster-name $SHARED_SERVICES_NAME \
    --tanzu-kubernetes-cluster-namespace $SHARED_SERVICES_NS \
    --insecure-skip-tls-verify \
    -u $SUPERVISOR_USERNAME
@@ -38,6 +53,11 @@ kubectl config use-context $SHARED_SERVICES_NS
 kubectl create clusterrolebinding psp:authenticated --clusterrole=psp:vmware-system-privileged --group=system:authenticated
 # Cert Manager and ClusterIssuer
 kubectl apply -f cd/apps/extensions-cert-manager
+while kubectl get po -n cert-manager | grep Running | wc -l | grep 3 ; [ $? -ne 0 ]; do
+    echo Cert Manager is not yet ready
+    sleep 5s
+done
+sleep 10 # seems to be needed to allow cert manager pods to startup
 cp manifests/shared-cluster-issuer-dns.yaml generated/shared/
 yq write generated/shared/shared-cluster-issuer-dns.yaml -i "spec.acme.solvers[0].dns01.route53.region" $(yq r $VARS_YAML aws.region)
 yq write generated/shared/shared-cluster-issuer-dns.yaml -i "spec.acme.solvers[0].dns01.route53.hostedZoneID" $(yq r $VARS_YAML aws.hostedZoneId)
@@ -45,13 +65,14 @@ yq write generated/shared/shared-cluster-issuer-dns.yaml -i "spec.acme.solvers[0
 kubectl create secret generic route53-credentials --from-literal=secret-access-key=$(yq r $VARS_YAML aws.secretKey) -n cert-manager
 kubectl apply -f generated/shared/shared-cluster-issuer-dns.yaml
 #ExternalDNS
-helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add tac https://charts.trials.tac.bitnami.com/demo
 cp manifests/values-external-dns.yaml generated/shared/
 yq write generated/shared/values-external-dns.yaml -i "aws.credentials.secretKey" $(yq r $VARS_YAML aws.secretKey)
 yq write generated/shared/values-external-dns.yaml -i "aws.credentials.accessKey" $(yq r $VARS_YAML aws.accessKey)
 yq write generated/shared/values-external-dns.yaml -i "aws.region" $(yq r $VARS_YAML aws.region)
 yq write generated/shared/values-external-dns.yaml -i "txtOwnerId" $(yq r $VARS_YAML aws.hostedZoneId)
-helm install external-dns-aws bitnami/external-dns -f generated/shared/values-external-dns.yaml
+helm install external-dns-aws tac/external-dns --version 3.5.0 -f generated/shared/values-external-dns.yaml
 #Wait for pod to be ready
 while kubectl get po -l app.kubernetes.io/name=external-dns -n default | grep Running ; [ $? -ne 0 ]; do
 	echo external-dns is not yet ready
@@ -88,12 +109,12 @@ kubectl create serviceaccount argocd -n argocd
 kubectl create clusterrolebinding argocd --clusterrole=cluster-admin --serviceaccount=argocd:argocd
 export TOKEN_SECRET=$(kubectl get serviceaccount -n argocd argocd -o jsonpath='{.secrets[0].name}')
 export TOKEN=$(kubectl get secret -n argocd $TOKEN_SECRET -o jsonpath='{.data.token}' | base64 --decode)
-kubectl config set-credentials $SHARED_SERVICES_NS-argocd-token-user --token $TOKEN
-kubectl config set-context $SHARED_SERVICES_NS-argocd-token-user@$SHARED_SERVICES_NS \
+kubectl config set-credentials $SHARED_SERVICES_NAME-argocd-token-user --token $TOKEN
+kubectl config set-context $SHARED_SERVICES_NAME-argocd-token-user@$SHARED_SERVICES_NAME \
   --user $SHARED_SERVICES_NS-argocd-token-user \
-  --cluster $(kubectl config get-contexts shared-services --no-headers | awk '{print $3}')
+  --cluster $(kubectl config get-contexts $SHARED_SERVICES_NAME --no-headers | awk '{print $3}')
 argocd cluster add #Dump configs
-argocd cluster add $SHARED_SERVICES_NS-argocd-token-user@$SHARED_SERVICES_NS
+argocd cluster add $SHARED_SERVICES_NAME-argocd-token-user@$SHARED_SERVICES_NAME
 #argocd cluster add $SUPERVISOR_VIP  #Add Supervisor Cluster
 argocd cluster list 
 
@@ -145,5 +166,44 @@ while kubectl get tanzukubernetesclusters $(yq r $VARS_YAML workload2.name) -n $
 done
 
 #Add Workloads to ArgoCD
+export WORKLOAD1_NAME=$(yq r $VARS_YAML workload1.name)
+export WORKLOAD1_NAMESPACE=$(yq r $VARS_YAML workload1.namespace)
+kubectl vsphere login -v 8 --server=$SUPERVISOR_VIP \
+   --tanzu-kubernetes-cluster-name $WORKLOAD1_NAME \
+   --tanzu-kubernetes-cluster-namespace $WORKLOAD1_NAMESPACE \
+   --insecure-skip-tls-verify \
+   -u $SUPERVISOR_USERNAME
+kubectl config use-context $WORKLOAD1_NAME
+kubectl create serviceaccount argocd -n argocd
+kubectl create clusterrolebinding argocd --clusterrole=cluster-admin --serviceaccount=argocd:argocd
+export TOKEN_SECRET=$(kubectl get serviceaccount -n argocd argocd -o jsonpath='{.secrets[0].name}')
+export TOKEN=$(kubectl get secret -n argocd $TOKEN_SECRET -o jsonpath='{.data.token}' | base64 --decode)
+kubectl config set-credentials $WORKLOAD1_NAME-argocd-token-user --token $TOKEN
+kubectl config set-context $WORKLOAD1_NAME-argocd-token-user@$WORKLOAD1_NAME \
+  --user $WORKLOAD1_NAME-argocd-token-user \
+  --cluster $(kubectl config get-contexts $WORKLOAD1_NAME --no-headers | awk '{print $3}')
+argocd cluster add #Dump configs
+argocd cluster add $WORKLOAD1_NAME-argocd-token-user@$WORKLOAD1_NAME
+argocd cluster list
+
+export WORKLOAD2_NAME=$(yq r $VARS_YAML workload1.name)
+export WORKLOAD2_NAMESPACE=$(yq r $VARS_YAML workload1.namespace)
+kubectl vsphere login -v 8 --server=$SUPERVISOR_VIP \
+   --tanzu-kubernetes-cluster-name $WORKLOAD2_NAME \
+   --tanzu-kubernetes-cluster-namespace $WORKLOAD2_NAMESPACE \
+   --insecure-skip-tls-verify \
+   -u $SUPERVISOR_USERNAME
+kubectl config use-context $WORKLOAD2_NAME
+kubectl create serviceaccount argocd -n argocd
+kubectl create clusterrolebinding argocd --clusterrole=cluster-admin --serviceaccount=argocd:argocd
+export TOKEN_SECRET=$(kubectl get serviceaccount -n argocd argocd -o jsonpath='{.secrets[0].name}')
+export TOKEN=$(kubectl get secret -n argocd $TOKEN_SECRET -o jsonpath='{.data.token}' | base64 --decode)
+kubectl config set-credentials $WORKLOAD2_NAME-argocd-token-user --token $TOKEN
+kubectl config set-context $WORKLOAD2_NAME-argocd-token-user@$WORKLOAD2_NAME \
+  --user $WORKLOAD2_NAME-argocd-token-user \
+  --cluster $(kubectl config get-contexts $WORKLOAD2_NAME --no-headers | awk '{print $3}')
+argocd cluster add #Dump configs
+argocd cluster add $WORKLOAD2_NAME-argocd-token-user@$WORKLOAD2_NAME
+argocd cluster list
 
 # Bootstrap workloads App-of-Apps
--- scripts/delete.sh
+++ scripts/delete.sh
@@ -3,9 +3,15 @@
 echo config YAML:
 cat $VARS_YAML
 
+export SUPERVISOR_VIP=$(yq r $VARS_YAML vsphere.supervisor-vip)
+export SUPERVISOR_USERNAME=$(yq r $VARS_YAML vsphere.username)
+export SUPERVISOR_PWD=$(yq r $VARS_YAML vsphere.password)
+kubectl vsphere login -v 8 --server=$SUPERVISOR_VIP --insecure-skip-tls-verify -u $SUPERVISOR_USERNAME
+kubectl config use-context $SUPERVISOR_VIP
+
 # Delete workloads
 helm uninstall $(yq r $VARS_YAML workload1.name) --namespace $(yq r $VARS_YAML workload1.namespace)
 helm uninstall $(yq r $VARS_YAML workload2.name) --namespace $(yq r $VARS_YAML workload2.namespace)
 
 # Delete Shared-services
-kubectl delete -f generated/shared/shared-services.yaml
+helm uninstall $(yq r $VARS_YAML shared-services.name) --namespace $(yq r $VARS_YAML shared-services.namespace)
