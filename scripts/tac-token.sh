a062a1c3b76e8ec8b3ab3bdfb74f3fbcbf7f668b
adamzwickey <azwickey@vmware.com>
2020-11-05 10:14:15 -0500

    added helper to get tac token

---
+++ scripts/tac-token.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+kubectl get secret $(kubectl get serviceaccount kubeapps-operator -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | grep kubeapps-operator-token) -o jsonpath='{.data.token}' -o go-template='{{.data.token | base64decode}}' | pbcopy
+echo "*************************"
+echo "Token copied to clipboard"
+echo "*************************"
